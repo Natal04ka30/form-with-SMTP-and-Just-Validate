403d75816bfdd25ae9550a8a772c1f9e2bf67651
Natal04ka30 <106341548+Natal04ka30@users.noreply.github.com>
2023-08-26 00:14:07 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -18,8 +18,8 @@ $mail->SMTPDebug = 3;                               // Enable verbose debug outp
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'tatarbeevanatala@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'ltljvyltibsyxcdo'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'natala@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '********'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
